0ae0da6f1adbafc1153f3162af14e2fb6049cb14
Syafiqahsafitri-ops <syafiqahsafitri@gmail.com>
2026-09-14 22:47:14 +0700

    First commit

---
+++ src/Utama.java
@@ -0,0 +1,32 @@
+public class Utama 
+{
+    public static void main(String[] args) 
+    {
+
+       int umur = 20;
+       double tinggiBadan = 168.5;
+       char nilaiHuruf = 'A';
+       boolean lulus = true;
+       
+       int A= 10;
+       int B= 3;
+       int penjumlahan= A + B; 
+       float pembagian= (float) A/B;
+       long a= 5000;
+       long b= 1000000;
+       long perkalian= a*b;
+       
+
+
+    System.out.println ("Umur : " + umur);
+    System.out.println ("Tinggi Badan = " + tinggiBadan);
+    System.out.println("Nilai Huruf = " + nilaiHuruf);
+    System.out.println("Lulus = " + lulus );
+    System.out.println("Hasil Penjumlahan = " + penjumlahan);
+    System.out.println("Hasil Pembagian = " + pembagian);
+    System.out.println("Hasil Perkalian = " + perkalian);
+    System.out.println("Hello, World!");
+
+
+    }
+}
